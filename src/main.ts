e294389b24bb493af8e0c336923eabccdd175a8c
Johan Mickaël <johan.rakotoniaina@mevia.fr>
2024-04-25 16:49:14 +0200

    Update map

--- src/main.ts
+++ src/main.ts
@@ -4,26 +4,14 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    WA.room.area.onEnter('repoInfoZone').subscribe(() => {
+        console.log('Entered Repo Zone')
     })
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
-    WA.room.area.onEnter('Github').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("GithubPopup", "It's " + time, []);
-    })
-    WA.room.area.onLeave('Github').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -31,12 +19,5 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
--- vite.config.ts
+++ vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     },
     plugins: [...getMapsOptimizers(maps, optimizerOptions)],
     server: {
-        host: "localhost",
+        host: "0.0.0.0",
         headers: {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
